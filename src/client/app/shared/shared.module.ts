036a9edbe22823184d12563dc3af99631833f6d5
Jafar Yormahmadzoda <jafaryor@velox.biz>
2016-09-23 18:53:03 +0500

    fix

--- src/client/app/shared/shared.module.ts
+++ src/client/app/shared/shared.module.ts
@@ -5,7 +5,6 @@ import { RouterModule } from '@angular/router';
 
 import { ToolbarComponent } from './toolbar/index';
 import { NavbarComponent } from './navbar/index';
-import { NameListService } from './name-list/index';
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
@@ -21,7 +20,7 @@ export class SharedModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: SharedModule,
-      providers: [NameListService]
+      providers: []
     };
   }
 }
